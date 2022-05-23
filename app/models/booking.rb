559d7bbb0e6aacc025625aebdec448e5bbcfb579
Lydia Soula soulalydia1@gmail.com <soulalydia1@gmail.com>
2022-05-23 16:17:51 +0200

    validations models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :planet
   belongs_to :user
+
+  validates :status, presence: true
+  validates :end_date, presence: true
+  validates :start_date, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :date_of_birth, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :spirit_animal, presence: true
+  validates :astro_sign, presence: true
 end
