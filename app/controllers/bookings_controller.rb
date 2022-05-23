bf992c7dd0275c80c47b5c8d92c3024d04bbebd0
Lydia Soula soulalydia1@gmail.com <soulalydia1@gmail.com>
2022-05-23 17:15:25 +0200

    add changements

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,23 @@
 class BookingsController < ApplicationController
+
+  def new
+    @booking = Booking.new
+  end
+
+  def total_price
+    period = @booking.end_date - @booking.start_date
+    period * @booking.planet.price_per_day
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.planet = @planet
+    #redirect_to => view checkout completed
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
