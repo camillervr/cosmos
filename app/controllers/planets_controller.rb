5bdd9d1cc9ee6b6214f154324b1607fcf9a31ee6
Lydia Soula soulalydia1@gmail.com <soulalydia1@gmail.com>
2022-05-23 15:22:54 +0200

    routes

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,2 +1,34 @@
 class PlanetsController < ApplicationController
+  # before_action :set_list, only: [:show, :destroy]
+  def index
+    @planets = Planet.all
+  end
+
+  def show
+    @planet = Planet.find(params[:id])
+  end
+
+  def new
+    @planet = Planet.new
+  end
+
+  def create
+    @planet = Planet.new(planet_params)
+    if @planet.save
+      redirect_to planet_path(@planet)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @planet.destroy
+    redirect_to planets_path, status: :see_other
+  end
+
+  private
+
+  def planet_params
+    params.require(:planet).permit()
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
+  resources :planets do
+    resources :bookings, only: [:new, :create]
+  end
+  # resources :bookings, only
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
