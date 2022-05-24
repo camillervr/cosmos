6e0b4c0f3f7b6e33ced85f738e361c0adb65c9e3
rena kakon <renakakon@gmail.com>
2022-05-24 13:12:33 +0200

    seed-api-added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :planets
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :email, uniqueness: true
-  validates :first_name
-  validates :last_name
-  validates :date_of_birth
-  validates :address
-  validates :city
-  validates :spirit_animal
-  validates :astro_sign
 end
