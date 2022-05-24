e946a70d922deaa0d073c8c6e079fd81da606275
Lydia Soula soulalydia1@gmail.com <soulalydia1@gmail.com>
2022-05-24 16:59:58 +0200

    cloudinary

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,6 +1,7 @@
 class Planet < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_one_attached :photo
   OBJECT = ["Star", "Planet", "Dwarf Planet", "Asteroid", "Comet", "Moon"]
 
   validates :name, presence: true
