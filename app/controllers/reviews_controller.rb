c1b7b0a7ea2e12b7df702487b352a75a5811bd27
Camille Riviere <camille.riviere6@gmail.com>
2022-05-27 13:05:15 +0200

    reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,9 +4,14 @@ class ReviewsController < ApplicationController
     @booking = Booking.find(params[:booking_id])
     @planet = @booking.planet
     @review = Review.new(review_params)
+    @review.booking_id = @booking.id
     authorize @review
-    @review.save
-    redirect_to planet_path(@planet)
+    if @review.save
+      redirect_to planet_path(@planet)
+    else
+      render "planets/show", status: :unprocessable_entity
+    end
+    # ratings
   end
 
   private
@@ -14,4 +19,15 @@ class ReviewsController < ApplicationController
   def review_params
     params.require(:review).permit(:rating, :content)
   end
+
+  # def ratings
+  #   ratings = []
+  #   @planet.bookings.each do |booking|
+  #     booking.reviews.each do |review|
+  #      review.content
+  #      ratings << review.rating
+  #     end
+  #   end
+  #   ratings_average = ratings.inject{ |sum, n| sum + n }.to_f / ratings.size
+  # end
 end
