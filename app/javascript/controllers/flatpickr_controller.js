dc67df581dbe136012e2262d654a48cef7c197df
Camille Riviere <camille.riviere6@gmail.com>
2022-05-27 17:58:42 +0200

    new fixed

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -3,7 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr"
 
 // Connects to data-controller="flatpickr"
-import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 export default class extends Controller {
   connect() {
     flatpickr(".datepicker", {
